a96311926c0d24c85871da3bff4604f54b11d13a
26nsmall <26nsmall@gannacademy.org>
2025-09-03 13:27:28 -0400

    Commit

--- src/times_ten.py
+++ src/times_ten.py
@@ -1,6 +1,10 @@
 # Write your solution here
 def times_ten(start_index: int, end_index: int):
-    pass
+    D = {}
+    for i in range(start_index,end_index+1):
+        D[i]= i*10
+    return D
+
 
 
 if __name__ == "__main__":
